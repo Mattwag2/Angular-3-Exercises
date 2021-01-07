a161685ccd1a253d930532485d48afb243abd5ff
Matt Wagner <mattwagner@Matts-MacBook-Air.local>
2021-01-07 15:31:23 -0600

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Exercises: Angular Lesson 3';
+
+  color = 'green';
+  height = 0;
+  width = 0;
+  message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled: boolean = true;
+
+
+handleTakeOff() {
+  let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+  if (result) {
+     this.color = 'blue';
+     this.height = 10000;
+     this.width = 0;
+     this.message = 'Shuttle in flight.';
+     this.takeOffEnabled = false
+  }
+}
+
+handleLand(rocketImage) {
+  let result = window.confirm('Are you sure you want to land the shutttle?');
+  if (result) {
+     this.color = 'blue';
+     this.height = 0;
+     this.width = 0;
+     this.message = 'Shuttle has landed.';
+     rocketImage.style.botttom = "0";
+  }
+}
+
+handleAbort(rocketImage) {
+  let result = window.confirm('Are you sure you want to Abort?');
+  if (result) {
+     this.color = 'red';
+     this.height = 0;
+     this.width = 0;
+     this.message = 'MISSION ABORTED.';
+     rocketImage.style.botttom = '0px';
+
+  }
+}
+
+moveRocket(rocketImage, direction){
+  if (direction === 'right'){
+    let movement = parseInt(rocketImage.style.left)+10+'px';
+    rocketImage.style.left = movement;
+    this.width = this.width + 1000;
+  } else if (direction === 'left'){
+    let movement = parseInt(rocketImage.style.left)-10+'px';
+    rocketImage.style.left = movement;
+    this.width = this.width - 1000;
+  } else if (direction === 'up'){
+    let movement = parseInt(rocketImage.style.bottom)+10+'px';
+    rocketImage.style.bottom = movement;
+    this.height = this.height + 1000;
+  } else if (direction === 'down'){
+    let movement = parseInt(rocketImage.style.bottom)-10+'px';
+    rocketImage.style.bottom = movement;
+    this.height = this.height - 1000;
+  
+  }
+}
+}
